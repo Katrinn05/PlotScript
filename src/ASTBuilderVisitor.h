aa3f3f4c689381f2a3920e144ebdb0f18ccc4e0f
Anton Perakhod <148491502+perakhod@users.noreply.github.com>
2025-05-26 22:39:14 +0200

    Update ASTBuilderVisitor.h

--- src/ASTBuilderVisitor.h
+++ src/ASTBuilderVisitor.h
@@ -31,18 +31,19 @@ public:
     std::any visitEmbeddedFunctionBlock(PlotScriptParser::EmbeddedFunctionBlockContext* ctx) override;
     std::any visitExportStatement(PlotScriptParser::ExportStatementContext* ctx) override;
 
-    std::any visitCpp_funcDeclaration(PlotScriptParser::Cpp_funcDeclarationContext* ctx) override;
-    std::any visitCpp_funcReturnType(PlotScriptParser::Cpp_funcReturnTypeContext* ctx) override;
-    std::any visitCpp_typeSpecifier(PlotScriptParser::Cpp_typeSpecifierContext* ctx) override;
-    std::any visitCpp_paramList(PlotScriptParser::Cpp_paramListContext* ctx) override;
-    std::any visitCpp_parameter(PlotScriptParser::Cpp_parameterContext* ctx) override;
-    std::any visitCpp_funcBody(PlotScriptParser::Cpp_funcBodyContext* ctx) override;
-    std::any visitCpp_funcStatement(PlotScriptParser::Cpp_funcStatementContext* ctx) override;
-    std::any visitCpp_assignment(PlotScriptParser::Cpp_assignmentContext* ctx) override;
-    std::any visitCpp_varDeclaration(PlotScriptParser::Cpp_varDeclarationContext* ctx) override;
-    std::any visitCpp_return(PlotScriptParser::Cpp_returnContext* ctx) override;
-    std::any visitCpp_controlStructure(PlotScriptParser::Cpp_controlStructureContext* ctx) override;
-    std::any visitCpp_expr(PlotScriptParser::Cpp_exprContext* ctx) override;
-};
+//     std::any visitCpp_funcDeclaration(PlotScriptParser::Cpp_funcDeclarationContext* ctx) override;
+//     std::any visitCpp_funcReturnType(PlotScriptParser::Cpp_funcReturnTypeContext* ctx) override;
+//     std::any visitCpp_typeSpecifier(PlotScriptParser::Cpp_typeSpecifierContext* ctx) override;
+//     std::any visitCpp_paramList(PlotScriptParser::Cpp_paramListContext* ctx) override;
+//     std::any visitCpp_parameter(PlotScriptParser::Cpp_parameterContext* ctx) override;
+//     std::any visitCpp_funcBody(PlotScriptParser::Cpp_funcBodyContext* ctx) override;
+//     std::any visitCpp_funcStatement(PlotScriptParser::Cpp_funcStatementContext* ctx) override;
+//     std::any visitCpp_assignment(PlotScriptParser::Cpp_assignmentContext* ctx) override;
+//     std::any visitCpp_varDeclaration(PlotScriptParser::Cpp_varDeclarationContext* ctx) override;
+//     std::any visitCpp_return(PlotScriptParser::Cpp_returnContext* ctx) override;
+//     std::any visitCpp_controlStructure(PlotScriptParser::Cpp_controlStructureContext* ctx) override;
+//     std::any visitCpp_expr(PlotScriptParser::Cpp_exprContext* ctx) override;
+ };
 
 #endif
+
