9948dafb6ebc1aadab65fcb707a2ae39a504178d
Anton Perakhod <148491502+perakhod@users.noreply.github.com>
2025-05-28 01:33:10 +0200

    Update Plotter.h

--- src/Plotter.h
+++ src/Plotter.h
@@ -5,58 +5,77 @@
 #include <string>
 #include <utility>
 
-struct Color { unsigned char r, g, b; };
+// Simple RGB color representation (24-bit)
+struct Color {
+    unsigned char r, g, b;
+};
 
+// Plotter: renders 2D data series with grid, axes, labels, and exports as BMP
 class Plotter {
 public:
-    
+    // Constructor: set image dimensions (pixels)
     Plotter(int width = 800, int height = 600);
 
-    
-    void setRange(double xmin, double xmax, double ymin, double ymax);
+    // Optionally override automatic range detection
+    void setRange(double x0, double x1, double y0, double y1);
 
-    
-    void addSeries(const std::vector<std::pair<double,double>>& pts, bool connect = true);
+    // Add a series of (x, y) points; connect=true draws lines between consecutive points
+    void addSeries(const std::vector<std::pair<double, double>>& pts, bool connect = true);
 
-    
+    // Save the rendered plot to a BMP file (filename should end in ".bmp")
     void save(const std::string& filename);
 
 private:
+    // Internal struct for a data series
     struct Series {
-        std::vector<std::pair<double,double>> pts;
+        std::vector<std::pair<double, double>> pts;
         bool connect;
         Color color;
     };
 
-    int imgW, imgH;
-    int marginL, marginR, marginT, marginB;
-    std::vector<Series> seriesList;
+    int imgW, imgH;                // Image width and height in pixels
+    int marginL, marginR, marginT, marginB;  // Margins around the plot area
 
-    double xmin, xmax, ymin, ymax;
-    bool hasRange;
-    double dataMinX, dataMaxX, dataMinY, dataMaxY;
+    double xmin, xmax, ymin, ymax; // Data range for axes
+    bool hasRange;                 // True if range set manually
+    double dataMinX, dataMaxX;     // Min/max data values (auto-range)
+    double dataMinY, dataMaxY;
 
-    std::vector<std::vector<Color>> pixels;
+    std::vector<Series> seriesList;                // All added data series
+    std::vector<std::vector<Color>> pixels;        // Pixel buffer [row][col]
 
-    
+    // Initialize pixel buffer to white background
     void clearPixels();
 
-    
+    // Low-level drawing primitives
     void setPixel(int x, int y, Color c);
     void drawLine(int x0, int y0, int x1, int y1, Color c);
     void drawPoint(int x, int y, Color c);
 
-   
-    void drawGrid();
-    void drawAxes();
-    void drawSeries();
-    
+    // Coordinate transforms: data -> pixel
     int toPxX(double x) const;
     int toPxY(double y) const;
+
+    // Compute "nice" step size for grid lines
     double niceStep(double range) const;
 
-    
+    // Render grid lines and axes
+    void drawGrid();
+    void drawAxes();
+
+    // Render all series into pixel buffer
+    void drawSeries();
+
+    // Draw single character and string (using 5x7 bitmap font)
+    void drawChar(int x, int y, char c, Color col);
+    void drawString(int x, int y, const std::string &s, Color col);
+
+    // Draw numeric labels along X and Y axes
+    void drawLabels();
+
+    // Write pixel buffer to BMP file on disk
     void writeBMP(const std::string& fname);
 };
 
 #endif // PLOTTER_H
+
